7b7dc0707dfb93be8323ffaf14d26f009025010b
sala302b <sala302b@dr302b370911.ceines.com.br>
2019-02-25 08:57:57 -0300

    Ola

--- Desktop/Projeto Java/Three Kings/src/three/kings/Refrigerante.java
+++ Desktop/Projeto Java/Three Kings/src/three/kings/Refrigerante.java
@@ -2,12 +2,14 @@ public class Refrigerante {
     //1. Encapsulamento dos atributos (private)
     private String marca, tamanho;
     private double preco ;
+    private String nome,email,cpf,telefone,matricula;
     
     //2. Metodo construtor
     public Refrigerante() {
     }
     /*
     public Refrigerante (String cpf, String nome, double 7.50) {
+        String coca = null;
         this.marca = coca;
         this.tamanho = ;
         this.preco = 7.50;
@@ -32,13 +34,13 @@ public class Refrigerante {
     public void setEmail(String email) {
         this.email = email;
     }
-    public int getTelefone() {
+    public String getTelefone() {
         return telefone;
     }
     public void setTelefone(int telefone) {
         this.telefone = telefone;
     }
-    public int getMatricula() {
+    public String getMatricula() {
         return matricula;
     }
     
@@ -47,6 +49,6 @@ public class Refrigerante {
                 this.nome + "\nTelefone: " + this.telefone +
                 "\nE-mail: " + this.email );
     }
-    */
+   */ 
     
 }
